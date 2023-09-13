ce8fe6d5403898436a577a3380445084b40434b0
Reh05 <rehemachiona05@gmail.com>
2023-09-13 22:37:49 +0000

    mandatory tasks

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""defines a from_json_string function"""
+import json
+
+
+def from_json_string(my_str):
+    """return an object represented by json string"""
+    return json.loads(my_str)
